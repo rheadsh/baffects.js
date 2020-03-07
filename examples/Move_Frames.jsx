400c8bdb49d91d0f8d66db2553fbe513a50660dc
rheadsh <bbetoo@gmail.com>
2020-03-06 20:26:41 -0600

    examples update

--- examples/Move_Frames.jsx
+++ examples/Move_Frames.jsx
@@ -4,14 +4,12 @@ var generalDuration = 60;
 
 
 function setup() {
-	for (var i=0; i<40; i++) {
-		b.fill(b.random(), b.random(), b.random());
-		b.solid();
-	}
+    for (var i = 0; i < 40; i++) {
+        b.fill(b.random(), b.random(), b.random());
+        b.solid();
+    }
     var lays = b.getAllLayers();
-	b.moveFrames(lays, 6, 0);
+    b.moveFrames(lays, 6, 0);
 }
 
 b.go("Render", generalDuration);
-
-
--- examples/Shapes_Effects.jsx
+++ examples/Shapes_Effects.jsx
@@ -5,13 +5,13 @@ var n = 60;
 function setup() {
     b.cleanComp();
     b.background(0);
-    
-    for (var i=0; i<n; i++) {
-        b.stroke(0, i/n, 1);
-        var line = b.line(0, i*20, b.width, i*20);
-        var twist = b.shapeTwist(line.layer);
+
+    for (var i = 0; i < n; i++) {
+        b.stroke(0, i / n, 1);
+        var line = b.line(0, i * 20, b.width, i * 20);
+        var twist = b.shapeTwist(line);
         b.key(twist.angle, 0, 0);
-        b.key(twist.angle, i*0.333, i*100);
+        b.key(twist.angle, i * 0.333, i * 100);
         b.expression(twist.center, "wiggle(0.3, 100)");
     }
 };
